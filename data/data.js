126260d35d2a74d9106c1ba7b0777f5eb3cc7047
tmti20 <tmti20@gmail.com>
2019-05-11 21:46:34 -0400

    Added Class Size, AP Result datagroup to data.js

--- data/data.js
+++ data/data.js
@@ -49,7 +49,7 @@ let studentPerformanceACT = new DataGroup(
 );
 
 // Data sets
-studentPerformanceACT.addDataSet("satScores_2012","SAT Scores 2012 (Math)",["school_name", "sat_math_avg_score"]);
+studentPerformanceACT.addDataSet("satScores_2012","ACT Scores 2012 (Math)",["school_name", "sat_math_avg_score"]);
 studentPerformanceACT.addDataSet("actScores_2013","ACT Scores 2013",["school_name", "critical_reading_mean"]);
 studentPerformanceACT.addDataSet("satScores_2012","SAT Scores 2013 (Math)",["school_name", "mathematics_mean"]);
 studentPerformanceACT.addDataSet("actScores_2013","ACT Scores 2012",["school_name", "writing_mean"]);
@@ -66,6 +66,68 @@ dataGroups.push(studentPerformanceACT);
 
 
 
+
+
+// ----------------------------------------
+// Data group
+// ----------------------------------------
+// Student AP Results
+// This group serves to show student performance on AP Results
+// for several schools across multiple schools
+let studentApResults = new DataGroup(
+    "studentApResults",
+    "AP Results 2012",
+    "Compare AP Results of multiple schools",
+    true
+);
+
+// Data sets
+studentApResults.addDataSet("apResults_2012","AP Results 2012",["school_name", "num_of_ap_test_takers"]);
+studentApResults.addDataSet("apResults_2012","AP Results 2012",["num_of_ap_test_takers", "num_of_ap_total_exams_taken"]);
+studentApResults.addDataSet("apResults_2012","AP Results 2012 (Number of Test Takers)",["school_name", "num_of_ap_exams_passed"]);
+
+
+// Relevant tags
+studentApResults.setTags(["AP", "2012", "2010", "2015"]);
+// studentApResults.longDesc = "Some long description about this data group blah blah blah."
+
+// About
+studentApResults.longDesc = "Some long description about this data group blah blah blah."
+
+// Add
+dataGroups.push(studentApResults);
+
+
+
+
+// ----------------------------------------
+// Data group
+// ----------------------------------------
+// Student AP Results
+// This group serves to show student performance on AP Results
+// for several schools across multiple schools
+let classSize = new DataGroup(
+    "studentApResults",
+    "Class Size",
+    "Compare Class Size of multiple schools",
+    true
+);
+
+// Data sets
+classSize.addDataSet("classSizeB_2015","Class Size Report - Borough Level 2014-2015",["boro", "average_class_size"]);
+classSize.addDataSet("classSizeD_2015","Class Size - District Level- (class Size vs Grade Level)",["class_size", "grade_level"]);
+classSize.addDataSet("classSizeB_2015","Class Size - Borough Level (Borough vs Grade Level)",["boro", "grade_level"]);
+classSize.addDataSet("classSizeD_2015","Class Size - District Level- (# of students vs % of students)",["number_of_students", "percent_of_students_in"]);
+
+// Relevant tags
+classSize.setTags(["AP", "2012", "2010", "2015"]);
+// studentApResults.longDesc = "Some long description about this data group blah blah blah."
+
+// About
+classSize.longDesc = "Some long description about this data group blah blah blah."
+
+// Add
+dataGroups.push(classSize);
 
 
 // ----------------------------------------
@@ -88,7 +150,7 @@ studentGradOutcomes.addDataSet("gradOutcomes_2015","DYCD after-school programs",
 
 // Relevant tags
 studentGradOutcomes.setTags(["gradOutcomes", "2012", "2010", "2015"]);
-studentGradOutcomes.longDesc = "Some long description about this data group blah blah blah."
+// studentGradOutcomes.longDesc = "Some long description about this data group blah blah blah."
 
 // About
 studentGradOutcomes.longDesc = "Some long description about this data group blah blah blah."
